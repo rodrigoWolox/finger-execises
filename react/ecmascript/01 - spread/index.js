1ff73d9c69944d3c41e8042b736940fca03e6cf7
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-10-17 16:13:07 -0300

    My first test ;)

--- react/ecmascript/01 - spread/index.js
+++ react/ecmascript/01 - spread/index.js
@@ -1,8 +1,13 @@
-import { isArray } from './utils';
+import isArray from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(aPossibleArray) {
+  let result;
+  if (isArray(aPossibleArray)) {
+    result = Math.min(...aPossibleArray);
+  } else {
+    result = aPossibleArray;
+  }
+  return result;
 }
 
 export function copy() {
