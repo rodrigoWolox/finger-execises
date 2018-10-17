3b17dc6c92199613f3352bea5015efd4d6848895
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-10-17 18:06:39 -0300

    Hope is ok

--- react/ecmascript/01 - spread/index.js
+++ react/ecmascript/01 - spread/index.js
@@ -3,7 +3,9 @@ import isArray from './utils';
 export function min(...args) {
   let result;
   console.log(args);
-  if (isArray(args)) {
+  if (!args.length) {
+    result = undefined;
+  } else if (isArray(args)) {
     if (isArray(args[0])) {
       result = Math.min(...args[0]);
     } else {
