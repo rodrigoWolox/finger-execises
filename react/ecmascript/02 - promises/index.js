c49de414e31c23de713c61de13fda7ad3de2a1f5
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-10-18 15:14:33 -0300

    I promise something is wrong

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -1,7 +1,14 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const initialTime = Date.now();
+  return new Promise((resolve, reject) => {
+    if (Date.now() - initialTime < time) {
+      setInterval(() => resolve(Date.now() - initialTime), 500);
+    } else {
+      reject(new Error('This time is too much !'));
+    }
+  });
 }
 
 export function asyncDelay() {
